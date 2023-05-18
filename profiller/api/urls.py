ee70c311982946acfc9cb780bcaf30bc362f8aba
Fatih-Tarim <fatihtrm99@gmail.com>
2023-05-18 15:35:27 +0300

    Viewset & Routers ilk adım

--- profiller/api/urls.py
+++ profiller/api/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from profiller.api.views import ProfilViewSet
+from rest_framework.routers import DefaultRouter
 
-profil_list = ProfilViewSet.as_view({'get':'list'})
-profil_detay = ProfilViewSet.as_view({'get':'retrieve'})
+# profil_list = ProfilViewSet.as_view({'get':'list'})
+# profil_detay = ProfilViewSet.as_view({'get':'retrieve'})
+
+router = DefaultRouter()
+router.register(r'kullanici-profilleri',ProfilViewSet)
 
 urlpatterns = [
-    path('kullanici-profilleri/', profil_list, name='profiller'),
-    path('kullanici-profilleri/<int:pk>', profil_detay, name='profil-detay'),
+    path('',include(router.urls)),
+    # path('kullanici-profilleri/', profil_list, name='profiller'),
+    # path('kullanici-profilleri/<int:pk>', profil_detay, name='profil-detay'),
 ]
